0c06f20b3fc3795ca6ac5342cd86ce6539618051
MengyaoLyu <alremy@outlook.com>
2017-12-16 16:49:46 -0500

    add remote github

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+// app.get('/', (req, res) => {
+//     res.send({hi: 'there'});
+// });
+
+//client id:
+//client secret:
+passport.use(new GoogleStategy());
 
 const PORT = process.env.PORT || 5000;
 
